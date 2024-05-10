bc62d26e3625878694788b3d8f799a155206bcf8
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-10 23:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ class Person {
 
 class Student extends Person {
 	Person.nam = "Raj"
+	study(){
+		console.log(`${Person.nam} is studying`);
+	}
 }
 
 class Teacher extends Person {
 	Person.nam = "Raj"
+	teach(){
+		console.log(`${Person.nam} is teaching`);
+	}
 }
 
 // Do not change the code below this line
